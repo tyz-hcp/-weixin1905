5022cc1dd3a958e2992db7474df28fad1784e4ec
tyz-hcp <1632786767@qq.com>
2019-12-12 07:03:05 +0000

    php://input

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -39,7 +39,8 @@ class WxController extends Controller
     {
         $log_file = "wx.log";   //public
         //将接收的数据记录到日志文件
-        $data = data('Y-m-d H-i-s') . json_encode($_POST);
+        $xml = file_get_contents("php://input");
+        $data = data('Y-m-d H-i-s') . $xml;
         file_put_contents($log_file, $data, FILE_APPEND);   //追加写
     }
 
