403ea3c23b8c68732e9f125a0092f066d604afa3
13166560446 <2256006378@qq.com>
2019-12-19 13:49:18 +0800

    获取头像

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -13,6 +13,9 @@ class VoteController extends Controller
         $data=$this->getAccessToken($code);
         //获取用户信息
         $user_info=$this->getUserInfo($data['access_token'],$data['openid']);
+        //保存用户信息
+        $userinfo_key='h:u:'.$data['openid'];
+        Redis::hMset($userinfo_key,$user_info);
 
         //处理业务逻辑
         $openid=$user_info['openid'];
@@ -28,7 +31,10 @@ class VoteController extends Controller
         echo '投票总人数 :'.$total;echo "</br>";
         $members = Redis::zRange($key,0,-1,true); //获取所有投票人的openid
         foreach($members as $k=>$v){
-            echo '用户 :'.$k.'投票时间: '.date('Y-m-d H:i:s',$v);echo '</br>';
+            $u_k='h:u:'.$k;
+            $u=Redis::hgetall($u_k);
+            echo '<img src="'.$u['headimgurl'].'">';
+            //echo '用户 :'.$k.'投票时间: '.date('Y-m-d H:i:s',$v);echo '</br>';
         }
 
     }
